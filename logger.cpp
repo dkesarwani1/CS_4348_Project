dfad289c3ebc6c7210fca245c50a3aeffcc3e6ff
Deepansh Kesarwani <kesarwanideepansh@gmail.com>
2026-02-27 22:54:43 -0600

    Logger: add stdin loop and QUIT handling

--- logger.cpp
+++ logger.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
 int main(int argc, char* argv[]) {
     if (argc != 2) {
@@ -11,6 +12,11 @@ int main(int argc, char* argv[]) {
     if (!logFile.is_open()) {
         std::cerr << "Error: could not open log file.\n";
         return 1;
+    }
+
+    std::string line;
+    while (std::getline(std::cin, line)) {
+        if (line == "QUIT") break;
     }
 
     return 0;
