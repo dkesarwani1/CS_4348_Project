f972813f4c6ba97924e62c0d3c25e752c2396aff
Deepansh Kesarwani <kesarwanideepansh@gmail.com>
2026-02-27 19:09:35 -0600

    Logger: open logfile in append mode

--- logger.cpp
+++ logger.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include <fstream>
 
 int main(int argc, char* argv[]) {
     if (argc != 2) {
         std::cerr << "Usage: " << argv[0] << " <logfile>\n";
+        return 1;
+    }
+
+    std::ofstream logFile(argv[1], std::ios::app);
+    if (!logFile.is_open()) {
+        std::cerr << "Error: could not open log file.\n";
         return 1;
     }
 
