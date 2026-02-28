7ef73ae515d08ab4f05ffb15842501180d6e8558
Deepansh Kesarwani <kesarwanideepansh@gmail.com>
2026-02-27 22:59:48 -0600

    Logger: parse action and a message

--- logger.cpp
+++ logger.cpp
@@ -1,22 +1,36 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-
+#include <sstream>
+using namespace std;
 int main(int argc, char* argv[]) {
     if (argc != 2) {
-        std::cerr << "Usage: " << argv[0] << " <logfile>\n";
+        cerr << "Usage: " << argv[0] << " <logfile>\n";
         return 1;
     }
 
-    std::ofstream logFile(argv[1], std::ios::app);
+    ofstream logFile(argv[1], std::ios::app);
     if (!logFile.is_open()) {
         std::cerr << "Error: could not open log file.\n";
         return 1;
     }
 
     std::string line;
-    while (std::getline(std::cin, line)) {
+    while (std::getline(std::cin, line)) 
+    {
         if (line == "QUIT") break;
+        if (line.empty()) continue;
+        std::istringstream iss(line);
+        std::string action;
+        iss >> action;
+        if (action.empty()) continue;
+
+        string message;
+        getline(iss, message);
+        if (!message.empty() && message[0] == ' ')
+            message.erase(0, 1);
+        logFile << line << std::endl;
+        logFile.flush(); // Ensure line is written immediately
     }
 
     return 0;
