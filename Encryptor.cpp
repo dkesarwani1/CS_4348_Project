8e221c849b2911e4d5aab643ca757ad23c36cb8d
Deepansh Kesarwani <kesarwanideepansh@gmail.com>
2026-02-28 20:50:56 -0600

    Encryptor: implement the PASS command

--- Encryptor.cpp
+++ Encryptor.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
 #include <string>
-
+#include <sstream>
+using namespace std;
 int main() {
     std::string line;
+    std::string key;
 
-    while (std::getline(std::cin, line)) {
+    while (getline(cin, line)) 
+    {
         if (line == "QUIT") break;
+        if (line.empty()) continue;
+        istringstream iss(line);
+        string command;
+        iss >> command;
+        if (command == "PASS") {
+            iss >> key;
+            cout << "OK\n";
+        }
     }
 
     return 0;
