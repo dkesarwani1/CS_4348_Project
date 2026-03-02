6d5339f3b65c93a2b99d75bb2cc5a59ec2ad82c4
Deepansh Kesarwani <kesarwanideepansh@gmail.com>
2026-03-02 17:04:57 -0600

    Final changes in logger.cpp

--- logger.cpp
+++ logger.cpp
@@ -4,41 +4,41 @@
 #include <sstream>
 #include <ctime>
 #include <iomanip>
-
+using namespace std;
 int main(int argc, char* argv[]) {
     // Check for correct usage
     if (argc != 2) {
-        std::cerr << "Usage: " << argv[0] << " <logfile>\n";
+        cerr << "Usage: " << argv[0] << " <logfile>\n";
         return 1;
     }
 
     // Open file in append mode
-    std::ofstream logFile(argv[1], std::ios::app);
+    ofstream logFile(argv[1], ios::app);
     if (!logFile.is_open()) {
-        std::cerr << "Error: could not open log file.\n";
+        cerr << "Error: could not open log file.\n";
         return 1;
     }
 
-    std::string line;
+    string line;
 
-    while (std::getline(std::cin, line)) 
+    while (getline(cin, line)) 
     {
 
         if (line == "QUIT") break;
         if (line.empty()) continue;
-        std::istringstream iss(line);
-        std::string action;
+        istringstream iss(line);
+        string action;
         iss >> action;
 
         if (action.empty()) continue;
-        std::string message;
-        std::getline(iss, message);
+        string message;
+        getline(iss, message);
         if (!message.empty() && message[0] == ' ')
             message.erase(0, 1);
-        std::time_t now = std::time(nullptr);
-        std::tm local_tm = *std::localtime(&now);
-        std::ostringstream ts;
-        ts << std::put_time(&local_tm, "%Y-%m-%d %H:%M");
+        time_t now = time(nullptr);
+        tm local_tm = *localtime(&now);
+        ostringstream ts;
+        ts << put_time(&local_tm, "%Y-%m-%d %H:%M");
         logFile << ts.str() << " [" << action << "]";
 
         if (!message.empty())
